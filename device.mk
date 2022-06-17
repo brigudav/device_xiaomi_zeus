28e331237559e8e081cf64a4e3cac5583e982968
Alex <alexvl1972@hotmail.com>
2022-06-17 17:34:20 +1000

    add make libxml2

--- device.mk
+++ device.mk
@@ -76,12 +76,14 @@ PRODUCT_HOST_PACKAGES += \
 
 TARGET_RECOVERY_DEVICE_MODULES += \
     libion \
+    libxml2 \
     vendor.display.config@1.0 \
     vendor.display.config@2.0 \
     libdisplayconfig.qti
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
     $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so
