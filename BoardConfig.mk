e30186e2caed29dd8ad6f4176149ba3eaee2145e
Alexander <alexvl1972@hotmail.com>
2023-01-07 11:18:47 +1000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,7 +111,6 @@ TW_USE_TOOLBOX := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_DEFAULT_BRIGHTNESS := 1200
-TW_EXCLUDE_APEX := true
 TARGET_USES_MKE2FS := true
 TW_EXCLUDE_APEX := true
 TW_NO_CPU_TEMP := true
